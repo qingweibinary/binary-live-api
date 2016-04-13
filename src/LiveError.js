c910c4667e20404b23c208174677d7b67b44eba7
qingwei <qingwei@binary.com>
2016-04-13 15:42:52 +0800

    Save to host from gh

--- src/LiveError.js
+++
@@ -1,8 +0,0 @@
-export default class LiveError extends Error {
-    constructor(errorObj) {
-        super();
-        this.message = `Server Error: (${errorObj.code}) ${errorObj.message}, caused by ${errorObj.echo_req}`;
-        this.stack = (new Error()).stack;
-        this.name = this.constructor.name;
-    }
-}
--- src/index.js
+++
@@ -1,3 +0,0 @@
-export LiveEvents from './LiveEvents';
-export LiveApi from './LiveApi';
-export * as OAuth from './OAuth';
